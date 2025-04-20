d3c86f70559b80598bf1588eda086922ac63309f
Dawood0426 <rd535328@gmail.com>
2025-04-20 01:20:40 -0700

    ye b change ha
    ye b ha kuch

--- app/layout.js
+++ app/layout.js
@@ -8,11 +8,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-    <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-V78YVZ0GXM"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)};
   gtag('js', new Date());
 
   gtag('config', 'G-V78YVZ0GXM');
